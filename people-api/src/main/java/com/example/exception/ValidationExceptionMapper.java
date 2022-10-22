15a9825af26fe363befa65fcb511c976bae77875
Maksim <madzarmaksim@gmail.com>
2022-10-22 21:26:23 +0200

    Add ValidationExceptionMapper

---
+++ people-api/src/main/java/com/example/exception/ValidationExceptionMapper.java
@@ -0,0 +1,33 @@
+package com.example.exception;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    private String getPropertyName(Path path)
+    {
+        String[] split = path.toString().split("[.]");
+        return split[split.length - 1];
+    }
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception)
+    {
+        Map<String, String> errors = exception.getConstraintViolations().stream()
+                .collect(Collectors.toMap(v -> getPropertyName(v.getPropertyPath()), ConstraintViolation::getMessage));
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(errors).type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
+
