f1f9b383cc29bb7e2b75d92ed9c0ccf848edfecc
Maksim <madzarmaksim@gmail.com>
2022-10-28 16:58:57 +0200

    Add date validation

---
+++ medical-api/src/main/java/com/example/exception/validation/Date.java
@@ -0,0 +1,23 @@
+package com.example.exception.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = DateValidator.class)
+@Documented
+public @interface Date {
+
+    String message() default "Date could not be parsed!";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
---
+++ medical-api/src/main/java/com/example/exception/validation/DateValidator.java
@@ -0,0 +1,61 @@
+package com.example.exception.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.text.SimpleDateFormat;
+
+public class DateValidator implements ConstraintValidator<Date, String> {
+
+    @Override
+    public void initialize(Date constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String date, ConstraintValidatorContext constraintValidatorContext) {
+        if (date == null ) {
+            return true;
+        }
+        try {
+            java.util.Date d = new SimpleDateFormat("dd-MM-yyyy").parse(date);
+            return true;
+        } catch (Exception e) {
+            try {
+                java.util.Date d = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+                return true;
+            } catch (Exception e1) {
+                try {
+                    java.util.Date d = new SimpleDateFormat("dd.MM.yyyy").parse(date);
+                    return true;
+                } catch (Exception e2) {
+                    try {
+                        java.util.Date d = new SimpleDateFormat("ddMMyyyy").parse(date);
+                        return true;
+                    } catch (Exception e3) {
+                        try {
+                            java.util.Date d = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                            return true;
+                        } catch (Exception e4) {
+                            try {
+                                java.util.Date d = new SimpleDateFormat("yyyy/MM/dd").parse(date);
+                                return true;
+                            } catch (Exception e5) {
+                                try {
+                                    java.util.Date d = new SimpleDateFormat("yyyy.MM.dd").parse(date);
+                                    return true;
+                                } catch (Exception e6) {
+                                    try {
+                                        java.util.Date d = new SimpleDateFormat("yyyyMMdd").parse(date);
+                                        return true;
+                                    } catch (Exception e7) {
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
