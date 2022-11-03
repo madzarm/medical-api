18ad3dde17e42f9193199fb608542e671b1672ba
Maksim <madzarmaksim@gmail.com>
2022-11-03 21:01:54 +0100

    Comment unused classes

--- people-api/src/main/java/com/example/exception/validation/UpdatePersonRequestValidator.java
+++ people-api/src/main/java/com/example/exception/validation/UpdatePersonRequestValidator.java
@@ -1,21 +1,21 @@
-package com.example.exception.validation;
-
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-import java.util.Objects;
-
-public class UpdatePersonRequestValidator implements ConstraintValidator<UpdatePersonRequestValidation, com.example.service.request.UpdatePersonRequest> {
-
-    @Override
-    public void initialize(UpdatePersonRequestValidation annotation) {
-    }
-
-    @Override
-    public boolean isValid(com.example.service.request.UpdatePersonRequest request, ConstraintValidatorContext constraintValidatorContext) {
-        boolean hasDHId = Objects.nonNull(request.getDiseaseHistoryId());
-        boolean hasDiseaseId = Objects.nonNull(request.getDiseaseId());
-        boolean hasDate = Objects.nonNull(request.getDateDiscovered());
-
-        return hasDHId == (hasDiseaseId || hasDate);
-    }
-}
+//package com.example.exception.validation;
+//
+//import javax.validation.ConstraintValidator;
+//import javax.validation.ConstraintValidatorContext;
+//import java.util.Objects;
+//
+//public class UpdatePersonRequestValidator implements ConstraintValidator<UpdatePersonRequestValidation, com.example.service.request.UpdatePersonRequest> {
+//
+//    @Override
+//    public void initialize(UpdatePersonRequestValidation annotation) {
+//    }
+//
+//    @Override
+//    public boolean isValid(com.example.service.request.UpdatePersonRequest request, ConstraintValidatorContext constraintValidatorContext) {
+//        boolean hasDHId = Objects.nonNull(request.getDiseaseHistoryId());
+//        boolean hasDiseaseId = Objects.nonNull(request.getDiseaseId());
+//        boolean hasDate = Objects.nonNull(request.getDateDiscovered());
+//
+//        return hasDHId == (hasDiseaseId || hasDate);
+//    }
+//}
